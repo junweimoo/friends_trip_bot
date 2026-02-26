8c7f7dd2a42cb40557929f1187c94484a8bf4a2c
junwei.m <junweimoo@gmail.com>
2026-02-26 22:24:22 +0800

    chore: remove unused code

--- handlers/Handlers.cpp
+++ handlers/Handlers.cpp
@@ -1,5 +1,4 @@
 #include "Handlers.h"
-#include "../conversations/DemoConversation.h"
 #include "../conversations/RecordPaymentConversation.h"
 #include <iostream>
 #include <memory>
@@ -9,51 +8,6 @@
 namespace handlers {
 
 void registerHandlers(bot::Bot& bot, const Services& services, const Repositories& repos) {
-    // Register /start command handler
-    bot.registerCommandHandler("/start", [&bot](const bot::Message& msg) {
-        // std::cout << "Received /start from " << msg.chat_id << std::endl;
-        bot.sendMessage(msg.chat_id, "Hello! I am your Friends Trip Bot.");
-    });
-
-    // Register /menu command to demonstrate Inline Keyboard
-    bot.registerCommandHandler("/menu", [&bot](const bot::Message& msg) {
-        bot::InlineKeyboardMarkup keyboard;
-        bot::InlineKeyboardButton btn1{"Option 1", "opt_1"};
-        bot::InlineKeyboardButton btn2{"Option 2", "opt_2"};
-
-        // Add buttons in a single row
-        keyboard.inline_keyboard = {{btn1, btn2}};
-
-        bot.sendMessage(msg.chat_id, "Please choose an option:", &keyboard);
-    });
-
-    // Register /convo command to start DemoConversation
-    bot.registerCommandHandler("/convo", [&bot](const bot::Message& msg) {
-        auto convo = std::make_unique<bot::DemoConversation>(msg.chat_id, 0, msg.sender_id, bot);
-        bot.registerConversation(std::move(convo));
-
-        bot.sendMessage(msg.chat_id, "Please enter string 1:");
-    });
-
-    // Register callback handler to edit the message when a button is pressed
-    bot.registerCallbackHandler([&bot](const bot::CallbackQuery& query) {
-        // std::cout << "Received callback: " << query.data << " from " << query.sender_name << std::endl;
-
-        bot::InlineKeyboardMarkup keyboard;
-        bot::InlineKeyboardButton btn1{"Option 1", "opt_1"};
-        bot::InlineKeyboardButton btn2{"Option 2", "opt_2"};
-        keyboard.inline_keyboard = {{btn1, btn2}};
-
-        std::string newText = query.data + " pressed";
-        bot.editMessage(query.chat_id, query.message_id, newText, &keyboard);
-    });
-
-    // Register generic text handler
-    bot.registerTextHandler([&bot](const bot::Message& msg) {
-        // std::cout << "Received text from " << msg.chat_id << ": " << msg.text << std::endl;
-        bot.sendMessage(msg.chat_id, "You said: " + msg.text);
-    });
-
     // register handler
     bot.registerCommandHandler("/register", [&bot, &userService = services.userService](const bot::Message& msg) {
         User user;
@@ -69,7 +23,7 @@ void registerHandlers(bot::Bot& bot, const Services& services, const Repositorie
         }
     });
 
-    // pay handler
+    // record payment handler
     bot.registerCommandHandler("/pay", [&bot, &repos](const bot::Message& msg) {
         auto convo = std::make_unique<RecordPaymentConversation>(
             msg.chat_id, 0, msg.sender_id,
